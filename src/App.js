1b5c7a310f78eae64b6b3f36fc3c69b6e2a13f9f
Victoria <sivosha2006@mail.ru>
2021-10-21 20:07:29 +0300

    added reduxe, reducer func

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+
+function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash)
+
+  const addCash = (cash) => {
+    dispatch({type:"ADD_CASH", payload: cash})
+  }
+
+  const getCash = (cash) => {
+    dispatch({ type: "GET_CASH", payload: cash});
+  };
+
+  return (
+    <div className={"app"}>
+      <div style={{fontSize:'3rem'}}>{cash}</div>
+      <div style={{ display: "flex" }}>
+        <button type="button" onClick={() => addCash(+prompt())}>Пополнить счет</button>
+        <button type="button" onClick={() => getCash(+prompt())}>Снять со счета</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
